cd60391e54f1876a52a4ae251663213aedce62f9
Riyane El Qoqui <riyane.dev@gmail.com>
2026-04-04 06:19:17 +0200

    bindings(java): add RxMsgView for managing read-only message views

---
+++ bindings/java/src/main/io/tachyon/RxMsgView.java
@@ -0,0 +1,34 @@
+package io.tachyon;
+
+import java.lang.foreign.MemorySegment;
+
+public final class RxMsgView {
+	private MemorySegment data;
+	private final int typeId;
+	private final long actualSize;
+
+	RxMsgView(MemorySegment rawPointer, int typeId, long actualSize) {
+		this.typeId = typeId;
+		this.actualSize = actualSize;
+		this.data = rawPointer.reinterpret(actualSize).asReadOnly();
+	}
+
+	public MemorySegment getData() {
+		if (data == null) {
+			throw new IllegalStateException("RxMsgView invalidated after batch commit");
+		}
+		return data;
+	}
+
+	public int getTypeId() {
+		return typeId;
+	}
+
+	public long getActualSize() {
+		return actualSize;
+	}
+
+	void invalidate() {
+		this.data = null;
+	}
+}
