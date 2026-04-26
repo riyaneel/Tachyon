e131b31e1e2e1cffa17860349fccc11cb96e762c
Riyane El Qoqui <riyane.dev@gmail.com>
2026-04-26 13:11:22 +0200

    fix(bindings/go): disable AVX-512 support in cgo flags

--- bindings/go/tachyon/cgo.go
+++ bindings/go/tachyon/cgo.go
@@ -4,7 +4,7 @@ package tachyon
 
 /*
 #cgo CFLAGS:   -I${SRCDIR}/_core_local/include
-#cgo CXXFLAGS: -std=c++23 -O3 -march=native -fno-exceptions -fno-rtti
+#cgo CXXFLAGS: -std=c++23 -O3 -march=native -mno-avx512f -fno-exceptions -fno-rtti
 #cgo CXXFLAGS: -fvisibility=hidden -Wall -Wextra
 #cgo CXXFLAGS: -I${SRCDIR}/_core_local/include
 
