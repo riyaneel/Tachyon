7c1fa24366f453e9103a0b17ac4d7b6985e89af6
Riyane El Qoqui <riyane.dev@gmail.com>
2026-05-02 23:38:47 +0200

    feat(go): include tachyon_rpc.cpp in Go bindings

--- bindings/go/tachyon/tachyon_bridge.cpp
+++ bindings/go/tachyon/tachyon_bridge.cpp
@@ -1,4 +1,5 @@
 #include "_core_local/src/arena.cpp"
 #include "_core_local/src/shm.cpp"
 #include "_core_local/src/tachyon_c.cpp"
+#include "_core_local/src/tachyon_rpc.cpp"
 #include "_core_local/src/transport_uds.cpp"
