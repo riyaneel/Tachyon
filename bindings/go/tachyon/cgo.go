d0188619c2322ebd2e99d88b10109c878aa73f2b
Riyane El Qoqui <riyane.dev@gmail.com>
2026-05-15 01:37:46 +0200

    build(go): refine CXXFLAGS for compatibility

--- bindings/go/tachyon/cgo.go
+++ bindings/go/tachyon/cgo.go
@@ -4,9 +4,11 @@ package tachyon
 
 /*
 #cgo CFLAGS:   -I${SRCDIR}/_core_local/include
-#cgo CXXFLAGS: -std=c++23 -O3 -march=native -mno-avx512f -fno-exceptions -fno-rtti
+#cgo CXXFLAGS: -std=c++23 -O3 -march=native -mtune=native -fno-exceptions -fno-rtti
 #cgo CXXFLAGS: -fvisibility=hidden -Wall -Wextra
 #cgo CXXFLAGS: -I${SRCDIR}/_core_local/include
+
+#cgo amd64 CXXFLAGS: -mno-avx512f
 
 #cgo linux  LDFLAGS: -lrt -lstdc++
 #cgo darwin LDFLAGS: -lc++
